6acad97cb78fe6750ff8722f9ced7127f2478d51
Staravora <staravora@protonmail.com>
2025-01-17 18:53:45 +0700

    added neovim config to home manager

--- home.nix
+++ home.nix
@@ -44,17 +44,14 @@
     };
   };   
 
-    #Neovim config
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      extraPackages = with pkgs; [
-        git
-      ];
-  
-      # Configure to use your existing config directory
-      configDir = ./nvim;
-    };
+  #Neovim config
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      git
+    ];
+  }; # Add this closing brace
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -67,8 +64,6 @@
     pkgs.nodejs
     pkgs.gcc
 
-  #];
-
   ] ++ (with pkgs.gnomeExtensions; [
     arcmenu
     blur-my-shell
@@ -79,10 +74,6 @@
     open-bar
     top-bar-organizer
   ]);
-
-  programs.gnome-shell = {
-    enable = true;
-  };
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -98,23 +89,26 @@
     # '')
 
   
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-
-    
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-    
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/nvim" = {
+      source = builtins.path {
+        name = "nvim-config";
+        path = ./nvim;
+      };
+      recursive = true;
+    };
   };
+
+  # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+  # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+  # # symlink to the Nix store copy.
+  # ".screenrc".source = dotfiles/screenrc;
+
+  # # You can also set the file content immediately.
+  # ".gradle/gradle.properties".text = ''
+  #   org.gradle.console=verbose
+  #   org.gradle.daemon.idletimeout=3600000
+  # '';
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
